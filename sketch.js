4ed94846b68cdb1501d1103856aaf137fd2aefbb
Luckystardance <mintliliez@gmail.com>
2016-08-25 11:25:47 -0500

    done

---
+++ sketch.js
@@ -0,0 +1,60 @@
+function setup() {
+  createCanvas(1200,1400);
+  background('#000000');
+
+  ellipseMode(CENTER);
+  fill(1000);
+  ellipse(100,100,50,50);
+
+  ellipse(1100,100,50,50);
+
+  ellipse(200,200,150,150);
+
+  ellipse(1000,200,150,150);
+
+  ellipse(400,400,300,300);
+
+  ellipse(800,400,300,300);
+
+    ellipseMode(CENTER);
+    fill(0);
+    ellipse(700,500,350,350)
+
+    ellipse(500,500,350,350)
+
+  ellipseMode(CENTER);
+  fill(1000);
+
+  ellipse(600,600,500,500);
+
+  ellipseMode(CENTER);
+  fill(0);
+  ellipse(420,750,300,300)
+
+  ellipse(770,750,300,300)
+
+
+  ellipseMode(CENTER);
+  fill(1000);
+  ellipse(400,800,300,300);
+
+  ellipse(800,800,300,300);
+
+  ellipse(200,600,125,125);
+
+  ellipse(1000,600,125,125);
+
+  ellipse(1100,700,50,50);
+
+  ellipse(100,700,50,50);
+
+  ellipse(600,1300,100,100);
+
+  ellipse(600,1100,100,100);
+
+  ellipse(600,900,100,100);
+
+  ellipse(600,700,100,100);
+
+
+}
