f9ea836019ff686c76a1b391b13987cfa349a394
Luckystardance <mintliliez@gmail.com>
2016-08-26 15:37:57 -0500

    EXTREMEdone

--- sketch.js
+++ sketch.js
@@ -16,23 +16,57 @@ function setup() {
 
   ellipse(800,400,300,300);
 
+  ellipse(600,730,400,400)
+
     ellipseMode(CENTER);
     fill(0);
     ellipse(700,500,350,350)
 
     ellipse(500,500,350,350)
 
+    ellipse(600,750,300,300)
+
   ellipseMode(CENTER);
   fill(1000);
-
   ellipse(600,600,500,500);
 
-  ellipseMode(CENTER);
-  fill(0);
-  ellipse(420,750,300,300)
+  ellipse(750,1000,130,130);
 
-  ellipse(770,750,300,300)
+  ellipse(450,1000,130,130);
 
+  ellipse(420,1140,100,100)
+
+  ellipse(770,1140,100,100)
+
+  ellipse(820,1250,75,75)
+
+  ellipse(380,1250,75,75)
+
+  ellipse(860,1350,50,50)
+
+  ellipse(340,1350,50,50)
+
+    ellipseMode(CENTER);
+    fill(0);
+    ellipse(420,750,300,300);
+
+    ellipse(770,750,300,300);
+
+    ellipse(500,1000,130,130);
+
+    ellipse(700,1000,130,130);
+
+    ellipse(750,1140,100,100);
+
+    ellipse(450,1140,100,100);
+
+    ellipse(800,1250,75,75)
+
+    ellipse(400,1250,75,75)
+
+    ellipse(850,1350,50,50)
+
+    ellipse(350,1350,50,50)
 
   ellipseMode(CENTER);
   fill(1000);
@@ -44,17 +78,34 @@ function setup() {
 
   ellipse(1000,600,125,125);
 
-  ellipse(1100,700,50,50);
+  ellipse(1130,600,25,25);
 
-  ellipse(100,700,50,50);
+  ellipse(70,600,25,25);
 
-  ellipse(600,1300,100,100);
+  ellipse(600,1350,55,55);
 
-  ellipse(600,1100,100,100);
+  ellipse(600,1250,80,80);
 
-  ellipse(600,900,100,100);
+  ellipse(600,1140,110,110);
 
-  ellipse(600,700,100,100);
+  ellipse(600,1000,150,150);
 
+  ellipse(600,250,50,50)
+
+  ellipse(950,1000,50,50)
+
+  ellipse(250,1000,50,50)
+
+  ellipse(1000,900,75,75)
+
+  ellipse(200,900,75,75)
+
+  ellipse(1050,1000,25,25)
+
+  ellipse(150,1000,25,25)
+
+  ellipse(1020,1100,75,75)
+
+  ellipse(180,1100,75,75)
 
 }
